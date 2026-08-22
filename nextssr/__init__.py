2d2893e89507ba0e74925d6125d1eec66e4f13cc
Fabiano Menegidio <labiomics@bioinformatica.com.br>
2026-08-22 12:24:41 -0300

    Export public API in nextssr package and update presentation website snippet

--- nextssr/__init__.py
+++ nextssr/__init__.py
@@ -2,4 +2,18 @@
 nextSSR - Next-generation Simple Sequence Repeat Identification Tool.
 """
 
+from nextssr.config import SSRConfig
+from nextssr.finder import SSRFinder
+from nextssr.primer import PrimerDesigner, PrimerPair
+from nextssr.epcr import EPCRSimulator, AmpliconResult
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "SSRConfig",
+    "SSRFinder",
+    "PrimerDesigner",
+    "PrimerPair",
+    "EPCRSimulator",
+    "AmpliconResult",
+]
